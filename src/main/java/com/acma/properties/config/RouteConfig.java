f555104ebae0c31f157132fdf06f7e006c06618d
any-random-admin <dipkvts@gmail.com>
2024-11-15 23:33:45 +0530

    Added filters in gateway

--- src/main/java/com/acma/properties/config/RouteConfig.java
+++ src/main/java/com/acma/properties/config/RouteConfig.java
@@ -3,10 +3,13 @@
  */
 package com.acma.properties.config;
 
+import java.time.Duration;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,15 +20,22 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RouteConfig {
 
-//	@Bean
-//	public RouteLocator routeDefinitions(RouteLocatorBuilder builder) {
-//		log.info("****Route Definitions Locator****");
-//		return builder.routes()
-//				
-//		.route("01-usermgmt-api-routes", (route->route.path("/users/**")
-//												  .uri("lb://ACMA-USERMGMT-SERVICE")
-//													    
-//											      ))
+		
+	@Bean
+	public RouteLocator routeDefinitions(RouteLocatorBuilder builder) {
+		log.info("****Route Definitions Locator****");
+		return builder.routes()
+				
+		.route("01-usermgmt-api-routes", (route->route.path("/acma/users/**")
+				.filters(filter->filter
+						 .addRequestHeader("userType", "agent")
+						 .rewritePath("/acma/users/?(?<segment>.*)", "/users/$\\{segment}")
+						 .addResponseHeader("userType", "agent")
+						 .localResponseCache(Duration.ofMinutes(3), DataSize.ofMegabytes(500)))
+				.uri("lb://ACMA-USERMGMT-SERVICE")
+												  
+													    
+											      ))
 //		.route("01a-usermgmt-document-routes", (route->route.path("/swagger-ui.html")//predicate
 //													        .uri("lb://ACMA-USERMGMT-SERVICE")))
 //		
@@ -37,8 +47,8 @@ public class RouteConfig {
 //		
 //		.route("02-authz-token-routes", (route->route.path("/token")
 //				                                      .uri("lb://ACMA-AUTHN-AUTHZ-SERVICE")))
-//		
-//		.build();
-//		
-//	}
+		
+		.build();
+		
+	}
 }
